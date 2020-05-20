501d86d5b60a7c5f7b8bfa9c1ca990288c8dcf5b
afrendeiro <afrendeiro@gmail.com>
2020-05-20 18:23:00 -0400

    add dev/testing requirements

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ def parse_requirements(req_file):
 
 
 REQUIREMENTS_FILE = "requirements.txt"
+DEV_REQUIREMENTS_FILE = "requirements.dev.txt"
 README_FILE = "README.md"
 
 
@@ -37,8 +38,7 @@ except ImportError:
 
 # Requirements
 requirements = parse_requirements(REQUIREMENTS_FILE)
-# requirements_test = parse_requirements("requirements/requirements.test.txt")
-# requirements_docs = parse_requirements("requirements/requirements.docs.txt")
+requirements_dev = parse_requirements(DEV_REQUIREMENTS_FILE)
 
 # Description
 long_description = open(README_FILE).read()
@@ -49,13 +49,14 @@ setup(
     name="imc",
     packages=find_packages(),
     use_scm_version={
-        'write_to': 'imc/_version.py',
-        'write_to_template': '__version__ = "{version}"\n'
+        "write_to": "imc/_version.py",
+        "write_to_template": '__version__ = "{version}"\n',
     },
     entry_points={
         "console_scripts": [
             "imcrunner = imcpipeline.runner:main",
-            "imcpipeline = imcpipeline.pipeline:main"]
+            "imcpipeline = imcpipeline.pipeline:main",
+        ]
     },
     description="A pipeline and utils for IMC data analysis.",
     long_description=long_description,
@@ -69,12 +70,15 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    keywords=",".join([
-        "computational biology",
-        "bioinformatics",
-        "imaging",
-        "mass cytometry",
-        "mass spectrometry"]),
+    keywords=",".join(
+        [
+            "computational biology",
+            "bioinformatics",
+            "imaging",
+            "mass cytometry",
+            "mass spectrometry",
+        ]
+    ),
     url="https://github.com/elementolab/hyperion-cytof",
     project_urls={
         "Bug Tracker": "https://github.com/elementolab/hyperion-cytof/issues",
@@ -84,13 +88,11 @@ setup(
     author=u"Andre Rendeiro",
     author_email="andre.rendeiro@pm.me",
     license="GPL3",
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     install_requires=requirements,
-    # tests_require=requirements_test,
-    # extras_require={
-    #     "testing": requirements_test,
-    #     "docs": requirements_docs},
-    # package_data={"imc": ["config/*.yaml", "templates/*.html", "models/*"]},
+    tests_require=requirements_dev,
+    extras_require={"dev": requirements_dev},
+    # package_data={"imc": ["config/*.yaml", "templates/*.html", "_models/*"]},
     data_files=[
         REQUIREMENTS_FILE,
         # "requirements/requirements.test.txt",
