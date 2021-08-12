96fd31f4639360fda1967f383279c34cf070104c
afrendeiro <afrendeiro@gmail.com>
2021-08-12 19:05:58 -0400

    patch Path object to support glob with ** pattern

--- imc/types.py
+++ imc/types.py
@@ -59,6 +59,18 @@ class Path(pathlib.Path):
         super().mkdir(mode=mode, parents=parents, exist_ok=exist_ok)
         return self
 
+    def glob(self, pattern: str) -> tp.Generator:
+        # to support ** with symlinks: https://bugs.python.org/issue33428
+        from glob import glob
+
+        if "**" in pattern:
+            sep = "/" if self.is_dir() else ""
+            yield from map(
+                Path,
+                glob(self.as_posix() + sep + pattern, recursive=True),
+            )
+        yield from super().glob(pattern)
+
 
 GenericType = tp.TypeVar("GenericType")
 
