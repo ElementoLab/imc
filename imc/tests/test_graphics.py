d39c8b23f09148b1a60d88f4911c21078ff7f487
afrendeiro <afrendeiro@gmail.com>
2021-12-15 19:03:32 -0500

    compatibility with matplotlib>=3.5

--- imc/tests/test_graphics.py
+++ imc/tests/test_graphics.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import pytest
+
+from matplotlib.image import AxesImage
+from matplotlib.legend import Legend
 
 
 class TestCellTypePlotting:
@@ -26,11 +26,13 @@ class TestCellTypePlotting:
         fig2 = roi.plot_cell_types()
 
         # Get arrays back from images
-        a1 = fig1.axes[0].get_children()[-3].get_array()
-        a2 = fig2.axes[0].get_children()[-3].get_array()
+        a1 = [i for i in fig1.axes[0].get_children() if isinstance(i, AxesImage)]
+        a1 = [a for a in a1 if len(a.get_array().shape) == 3][0].get_array()
+        a2 = [i for i in fig2.axes[0].get_children() if isinstance(i, AxesImage)]
+        a2 = [a for a in a2 if len(a.get_array().shape) == 3][0].get_array()
 
         # Get legend of second image
-        l2 = fig2.axes[0].get_children()[-2]
+        l2 = [i for i in fig2.axes[0].get_children() if isinstance(i, Legend)][0]
 
         # Get color of legend patch (RGBA)
         lc = l2.get_patches()[0].get_facecolor()[:-1]
